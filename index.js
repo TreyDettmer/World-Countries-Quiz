83962c583ce9a5ae9b62c0983cfc70f2402b6003
TreyDettmer <dettmertrey@gmail.com>
2021-12-21 20:54:43 -0800

    Prepared project for Heroku host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var path = require('path')
 const express = require('express')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log("app running")
 });
